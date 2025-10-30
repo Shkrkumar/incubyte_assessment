7c4c4819b722b75c71172973769ffee46e60e096
Shekhar Kumar <shekhar.rrj@gmail.com>
2025-10-30 16:55:18 +0530

    Add test for summing two numbers in StringCalculator#add method

--- spec/string_calculator_spec.rb
+++ spec/string_calculator_spec.rb
@@ -12,5 +12,10 @@ RSpec.describe StringCalculator do
       calculator = StringCalculator.new
       expect(calculator.add('1')).to eq(1)
     end
+
+    it 'returns the sum of two numbers separated by comma' do
+      calculator = StringCalculator.new
+      expect(calculator.add('1,2')).to eq(3)
+    end
   end
 end
